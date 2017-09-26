303a90df651f46004af471e456d8ce553c8003ce
JohanWikstrom <johan.wikstrom@lund.ntig.se>
2017-09-26 14:07:28 +0200

    Added Languages

--- Index.php
+++ Index.php
@@ -12,9 +12,13 @@
 
 
 <?php
-
+echo '<tr>';
 echo "<a href='Cities.PHP'>Cities</a"; 
+echo '</tr>';
+echo '<tr>';
 echo "<a href='Country.PHP'>Countries</a>"; 
+echo '</tr>';
+echo '<tr>';
 echo "<a href='Language.PHP'>Languages</a>"; 
 
 
